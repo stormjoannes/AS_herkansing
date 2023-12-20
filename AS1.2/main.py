8a6bd5c902a33721b13654f256751f31fd0891b1
stormjoannes <storm.joannes@student.hu.nl>
2023-12-20 22:26:20 +0100

    create maze

---
+++ AS1.2/main.py
@@ -0,0 +1,6 @@
+"""In this file we make sure everything is connected and run properly"""
+
+from maze import Maze
+from agent import Agent
+from policy import Policy
+
---
+++ AS2.2/Maze.py
@@ -0,0 +1,50 @@
+"""In this file we define create our maze"""
+
+
+class Maze:
+
+    def __init__(self):
+        self.rewards = {}
+        self.grid = {}
+        self.terminal_states = []
+        self.actions = {0: [-1, 0], 1: [1, 0], 2: [0, 1], 3: [0, -1]}
+
+    def stepper(self, position, action):
+        movement = self.actions[action]
+        new_position = (position[0] + movement[0], position[1] + movement[1])
+
+        if new_position in self.grid:
+            return new_position
+        else:
+            return position
+
+    def fillDict(self, value, sizeHorizontal=4, sizeVertical=4):
+        """
+        Filling a dictionary with coördinates.
+        """
+        index = 0
+        dict = {}
+        for vertical in range(sizeVertical):
+            for horizontal in range(sizeHorizontal):
+                coord = (vertical, horizontal)
+                dict[coord] = value[index]
+                index += 1
+
+        return dict
+
+    def create_maze_values(self):
+        values = [[0], [0], [0], [0],
+                  [0], [0], [0], [0],
+                  [0], [0], [0], [0],
+                  [0], [0], [0], [0]]
+
+        rewards = [-1, -1, -1, 40,
+                   -1, -1, -10, -10,
+                   -1, -1, -1, -1,
+                   10, -2, -1, -1]
+
+        self.terminal_states = [(0, 3), (3, 0)]
+
+        self.grid, self.rewards = self.fillDict(values), self.fillDict(rewards)
+
+
