57ba7a260979e38549b791a8daa7f1583cab7962
Storm Joannes <storm.joannes@oakk.nl>
2023-12-22 09:49:47 +0100

    done\ AS1.2

--- AS2.2/Policy.py
+++ AS2.2/Policy.py
@@ -26,6 +26,7 @@ class Policy:
         return new_value
 
     def monte_carlo(self, options, iteration):
+        """"""
         new_value = max(self.maze.rewards[options[0]] + (self.discount * self.maze.grid[options[0]][iteration]),
                         self.maze.rewards[options[1]] + (self.discount * self.maze.grid[options[1]][iteration]),
                         self.maze.rewards[options[2]] + (self.discount * self.maze.grid[options[2]][iteration]),
@@ -33,9 +34,9 @@ class Policy:
         return new_value
 
     def choose_action(self, position, surr_states):
+        """"""
         highest_state = max(surr_states, key=surr_states.get)
 
-        move = [highest_state[0] - position[0], highest_state[1] - position[1]]
-        action = [k for k, v in self.maze.actions.items() if v == move]
+        pos_action = [highest_state[0] - position[0], highest_state[1] - position[1]]
+        action = [key for key, action_position in self.maze.actions.items() if action_position == pos_action]
         return action
-
